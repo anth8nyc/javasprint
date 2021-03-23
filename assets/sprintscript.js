d8e28803100c1dd85538fb17ea44e1d3aae4fe22
Anthony Castillo <anthonycastillo821@gmail.com>
2021-03-23 14:35:23 -0500

    Added Question render functionality

--- assets/sprintscript.js
+++ assets/sprintscript.js
@@ -1,65 +1,101 @@
-
-// let mainEl = document.getElementById("main");
-
 // // Create Start Button function
-
-// startfunction()
-
-// function startfunction(){
-
-
-
-
-
-
-// Replicate function with rules explanation or build into previous function
-
 // Begin Timer function
-
+// Replicate function with rules explanation or build into previous function
  
 let startBtnEl = document.querySelector("#startBtn");
 let timerEl = document.querySelector(".timer");
 let mainEl = document.querySelector(".main");
+
 let questContainEl = document.querySelector(".questContain")
-let answerBtnEl = document.querySelector(".answerBtn")
 let questEl = document.querySelector(".question")
+
+let answerBtnEl = document.querySelector(".answerBtn")
+let answerBtnAEl = document.querySelector("#answerchoicea")
+let answerBtnBEl = document.querySelector("#answerchoiceb")
+let answerBtnCEl = document.querySelector("#answerchoicec")
+let answerBtnDEl = document.querySelector("#answerchoiced")
+
+let randomQuest
+let currentQuest = 0
+
+let questions = [
+  
+  {
+    question: "What's my favorite color?", 
+    answers:[
+      { text: "Red", correct: true},
+      { text: "Yellow", correct: false},
+      { text: "Blue", correct: false},
+      { text: "Green", correct: false},
+    ]},
+  {
+    question: "What's my favorite number?", 
+    answers:[
+      { text: "8", correct: false},
+      { text: "21", correct: false},
+      { text: "4", correct: true},
+      { text: "0", correct: false},
+    ]},
+  {
+    question: "What's my favorite food?", 
+    answers:[
+      { text: "Red", correct: false},
+      { text: "Yellow", correct: false},
+      { text: "Pizza", correct: true},
+      { text: "Burgers", correct: false},
+    ]},
+  
+]
 
 startBtnEl.addEventListener("click", function(){
   
   clearInterval(setTime);
-  let secondsLeft = 10;
   setTime();
   removeStartBtn();
   renderQuestions();
-  
-  for (var i = 0; i < answerBtnEl.length; i++) {
-    answerBtnEl[i].setAttribute("style", "display:flex");
-  }
-  
-  function setTime() {
-    var timerInterval = setInterval(function() {
-      timerEl.setAttribute("style", "display: flex")
-      secondsLeft--;
-      timerEl.textContent = secondsLeft + " seconds left";
-      
-      if(secondsLeft === 0) {
-        clearInterval(timerInterval);
-        endGame();
-      }
-    }, 1000);
-  }
-
-  function removeStartBtn() {
-    startBtnEl.setAttribute("style", "display:none")
-  }
-
+    
 });
 
+function setTime() {
+  let secondsLeft = 75;
+  var timerInterval = setInterval(function() {
+    timerEl.setAttribute("style", "display: flex")
+    secondsLeft--;
+    timerEl.textContent = secondsLeft + " seconds left";
+    
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      endGame();
+    }
+  }, 1000);
+}
+
+function removeStartBtn() {
+  startBtnEl.setAttribute("style", "display:none")
+}
 
 function renderQuestions(){
 
   questContainEl.setAttribute("style", "display: flex")
 
+  // for (let index = 0; index < array.length; index++) {
+  //   const element = array[index];
+    
+  // }
+
+  // For loop //
+  questEl.textContent = questions[currentQuest].question
+
+  answerBtnAEl.textContent = questions[currentQuest].answers[0].text
+  answerBtnBEl.textContent = questions[currentQuest].answers[1].text
+  answerBtnCEl.textContent = questions[currentQuest].answers[2].text
+  answerBtnDEl.textContent = questions[currentQuest].answers[3].text
+    // on click, check correct t/f 
+      //if else 
+      // either renders next question
+        // if correct add to score
+       // else subtract time
+  
 }
 
 
@@ -69,7 +105,7 @@ function endGame() {
 }
 
 
-// Begin Quiz UI function 
+// Begin Quiz function 
 
     // Each question should track the correct answer choice to the score 
     // and subtract time from the timer for incorrect answers
@@ -78,6 +114,3 @@ function endGame() {
 // include form in display page for initials input
 
 // use local storage to take information for display page to log high score
-
-
-
