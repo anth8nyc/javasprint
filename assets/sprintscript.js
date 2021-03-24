fc9490a231535a78a46261edc494979d5f7cb2e2
Anthony Castillo <anthonycastillo821@gmail.com>
2021-03-23 20:47:35 -0500

    fixed button UI hover functionality

--- assets/sprintscript.js
+++ assets/sprintscript.js
@@ -2,21 +2,23 @@
 // Begin Timer function
 // Replicate function with rules explanation or build into previous function
  
-let startBtnEl = document.querySelector("#startBtn");
+let startBtnEl = document.querySelector(".startBtn");
+let timeBtnEl = document.querySelector(".time");
 let timerEl = document.querySelector(".timer");
 let mainEl = document.querySelector(".main");
 
 let questContainEl = document.querySelector(".questContain")
 let questEl = document.querySelector(".question")
 
-let answerBtnEl = document.querySelector(".answerBtn")
+let answerBtnsEl = document.querySelectorAll(".answerBtn")
 let answerBtnAEl = document.querySelector("#answerchoicea")
 let answerBtnBEl = document.querySelector("#answerchoiceb")
 let answerBtnCEl = document.querySelector("#answerchoicec")
 let answerBtnDEl = document.querySelector("#answerchoiced")
 
-let randomQuest
-let currentQuest = 0
+let secondsLeft = 75;
+let score = 0;
+let currentQuest = 0;
 
 let questions = [
   
@@ -39,8 +41,8 @@ let questions = [
   {
     question: "What's my favorite food?", 
     answers:[
-      { text: "Red", correct: false},
-      { text: "Yellow", correct: false},
+      { text: "Tacos", correct: false},
+      { text: "Hot Dogs", correct: false},
       { text: "Pizza", correct: true},
       { text: "Burgers", correct: false},
     ]},
@@ -57,9 +59,8 @@ startBtnEl.addEventListener("click", function(){
 });
 
 function setTime() {
-  let secondsLeft = 75;
+  timeBtnEl.setAttribute("style", "display: flex")
   var timerInterval = setInterval(function() {
-    timerEl.setAttribute("style", "display: flex")
     secondsLeft--;
     timerEl.textContent = secondsLeft + " seconds left";
     
@@ -68,33 +69,59 @@ function setTime() {
       endGame();
     }
   }, 1000);
-}
+} 
 
 function removeStartBtn() {
   startBtnEl.setAttribute("style", "display:none")
 }
 
 function renderQuestions(){
-
-  questContainEl.setAttribute("style", "display: flex")
-
-  // for (let index = 0; index < array.length; index++) {
-  //   const element = array[index];
-    
-  // }
-
-  // For loop //
+  
+  questContainEl.setAttribute("style", "display: flex");
+  
   questEl.textContent = questions[currentQuest].question
-
+  
   answerBtnAEl.textContent = questions[currentQuest].answers[0].text
   answerBtnBEl.textContent = questions[currentQuest].answers[1].text
   answerBtnCEl.textContent = questions[currentQuest].answers[2].text
   answerBtnDEl.textContent = questions[currentQuest].answers[3].text
+  
+  
+  for (let currentQuest = 0; currentQuest < questions.length; currentQuest++) {
+    
+    answerBtnsEl.addEventListener("click", function(){
+    
+      let chosenQ = questions[currentQuest];
+      let chosenA = answerBtnsEl.currentTarget
+      console.log(chosenA)
+
+      if (this.correct === true) {
+        score++;
+        currentQuest++;
+      } else {
+        secondsLeft - 10;
+        currentQuest++;
+      }
+      
+    });
+      
+  }
+    
+  // for (let index = 0; index < array.length; index++) {
+    //   const element = array[index];
+    
+    // }
+    
+    // For loop //
     // on click, check correct t/f 
-      //if else 
-      // either renders next question
-        // if correct add to score
-       // else subtract time
+    //if else 
+    // either renders next question
+    // if correct add to score
+    // else subtract time
+    
+    // currentQuest++;    
+    // renderQuestions();  
+
   
 }
 
